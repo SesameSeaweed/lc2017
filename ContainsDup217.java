841afd61fb8252e2a827d1330a02806293ec93de
Peipei Xu <pxu@akamai.com>
2017-06-05 17:23:20 -0400

    solution for 217

---
+++ ContainsDup217.java
@@ -0,0 +1,20 @@
+import java.util.Set;
+import java.util.HashSet;
+
+public class ContainsDup217 {
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) {
+        	if(set.contains(num)) return true;
+        	else {
+        		set.add(num);
+        	}
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+    	int[] nums = {1,2,3,4,5,1};
+    	System.out.println(containsDuplicate(nums));
+    }
+}
